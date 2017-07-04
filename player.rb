bcdd7fd0739be64c3cc69517ec687e6cd2b75a78
Alex Bazlinton <abazlinton@gmail.com>
2017-07-04 21:59:12 +0100

    basic player tests pass

---
+++ player.rb
@@ -0,0 +1,16 @@
+class Player
+
+  attr_reader :name, :position
+
+  def initialize(name)
+    @name = name
+    @position = 0
+  end
+
+  def move(spaces)
+    @position += spaces
+
+  end
+
+
+end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -5,10 +5,22 @@ Minitest::Reporters.use!( Minitest::Reporters::SpecReporter.new )
 
 class PlayerTest < MiniTest::Test 
 
-  def test_player_has_name
+  def setup
+    @player = Player.new("Alex")
   end
 
-  def test_player_has_position
+
+  def test_player_has_name
+    assert_equal("Alex", @player.name)
+  end
+
+  def test_player_position_starts_at_zero
+    assert_equal(0, @player.position)
+  end
+
+  def test_player_can_move
+    @player.move(2)
+    assert_equal(2, @player.position)
   end
 
 
