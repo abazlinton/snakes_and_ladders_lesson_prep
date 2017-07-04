c6374401d6f23872893f3c866fcccbae8e1dbba6
Alex Bazlinton <abazlinton@gmail.com>
2017-07-04 21:54:43 +0100

    basic player_spec

---
+++ specs/player_spec.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require '../player'
+require( 'minitest/reporters' )
+Minitest::Reporters.use!( Minitest::Reporters::SpecReporter.new )
+
+class PlayerTest < MiniTest::Test 
+
+  def test_player_has_name
+  end
+
+  def test_player_has_position
+  end
+
+
+end
